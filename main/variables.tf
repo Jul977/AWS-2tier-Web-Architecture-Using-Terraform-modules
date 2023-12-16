3f52ef317875645c8efa65e4c4e1e4a1148f9133
JULIAN77 <julianigemokhai@gmail.com>
2023-12-16 18:25:28 +0100

    update

--- main/variables.tf
+++ main/variables.tf
@@ -7,11 +7,8 @@ variable "pri_web_az1_cidr" {}
 variable "pri_web_az2_cidr" {}
 variable "pri_db_az1_cidr" {}
 variable "pri_db_az2_cidr" {}
-
 variable "db_username" {}
 variable "db_password" {}
-
-
 variable "certificate_domain_name" {}
 variable "additional_domain" {}
 
